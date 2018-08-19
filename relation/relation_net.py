52fa83413adf41e0f217be9f80e11a645b276273
dowhilefalse <37575766+dowhilefalse@users.noreply.github.com>
2018-08-19 21:23:33 +0800

    Update relation_net.py

--- relation/relation_net.py
+++ relation/relation_net.py
@@ -4,3 +4,11 @@ def relation_net():
     def __init__():
         loc_labels, class_label = retinanet()
         features = tf.reshape()
+        
+        arg_scope = vgg_arg_scope()
+        with slim.arg_scope(arg_scope):
+           output_vgg16, _ = vgg16(input_image, is_training=True)
+        
+        
+        
+
