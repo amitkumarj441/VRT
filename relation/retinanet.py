f31a2d02fd87b38da5d31d590ea80c0b4547a911
dowhilefalse <37575766+dowhilefalse@users.noreply.github.com>
2018-08-19 18:20:03 +0800

    Update retinanet.py

--- relation/retinanet.py
+++ relation/retinanet.py
@@ -6,7 +6,16 @@ class RetinaNet():
      See: https://arxiv.org/pdf/1708.02002.pdf
     """
     def __init__(self):
-        self._retina_fpn = resnet101()
+        arg_scope = resnet_arg_scope()
+        with slim.arg_scope(arg_scope):
+           output, _ = resnet_v2_101(input_image, is_training=True)
+
+    #   sess=tf.Session()
+    #   checkpoint_path = 'resnet_v2_101.ckpt'
+    #   saver = tf.train.Saver(tf.global_variables)
+    #   saver.restore(sess, checkpoint_path)
+    #   sess.close()
+        self.resnet = output
 
     def __call__(self, inputs, num_classes, num_anchors=9, scope=None, reuse=None):
         """
@@ -33,19 +42,20 @@ class RetinaNet():
 
     def forward(self, inputs):
         batch_size = tf.shape(inputs)[0]
-        feature_maps = self._retina_fpn(inputs)
+        feature_maps = self.resnet
         loc_predictions = []
         class_predictions = []
-        for idx, feature_map in enumerate(feature_maps):
-            loc_prediction = self.add_fcn_head(feature_map,
-                                                self._num_anchors * 4,
-                                                "Box")
-            class_prediction = self.add_fcn_head(feature_map,
-                                                  self._num_anchors*self._num_classes,
-                                                  "Class")
-            loc_prediction = tf.reshape(loc_prediction, [batch_size, -1, 4])
-            class_prediction = tf.reshape(class_prediction, [batch_size, -1, self._num_classes])
-            loc_predictions.append(loc_prediction)
-            class_predictions.append(class_prediction)
+#         for idx, feature_map in enumerate(feature_maps):
+        loc_prediction = self.add_fcn_head(feature_map,
+                                            self._num_anchors * 4,
+                                            "Box")
+        class_prediction = self.add_fcn_head(feature_map,
+                                              self._num_anchors*self._num_classes,
+                                              "Class")
+        loc_prediction = tf.reshape(loc_prediction, [batch_size, -1, 4])
+        class_prediction = tf.reshape(class_prediction, [batch_size, -1, self._num_classes])
+        loc_predictions.append(loc_prediction)
+        class_predictions.append(class_prediction)
+        
         return tf.concat(loc_predictions, axis=1), tf.concat(class_predictions, axis=1)
       
