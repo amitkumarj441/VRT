f7cbfce15dfca48a1334b8bcb2245fb4a46bdf2f
dowhilefalse <37575766+dowhilefalse@users.noreply.github.com>
2018-08-19 17:33:51 +0800

    Create test_resnet.py
    grab these resnet code from tensorflow

---
+++ relation/test_resnet.py
@@ -0,0 +1,16 @@
+from resnet import *
+slim=tf.contrib.slim
+
+if __name__=='__main__':
+   arg_scope = resnet_arg_scope():
+   with slim.arg_scope(arg_scope):
+    output, _ = resnet_v2_50(input_image, is_training=True)
+    
+   sess=tf.Session()
+   checkpoint_path = 'resnet_v2_101.ckpt'
+   saver = tf.train.Saver(tf.global_variables)
+   saver.restore(sess, checkpoint_path)
+   print(output.shape)
+   
+   sess.close()
+   
