6a04ff7c5846373d9949fa39ea36c247c0a918d1
dowhilefalse <37575766+dowhilefalse@users.noreply.github.com>
2018-08-20 12:00:20 +0800

    Update relation_net.py

--- relation/relation_net.py
+++ relation/relation_net.py
@@ -1,7 +1,7 @@
 from retinanet import *
-from roi_pooling import *
 
-# def relation_net():
+from roi_pooling.roi_pooling_ops import roi_pooling
+
 def relation_net(locations, class_labels, relation_numbers, use_obj=True, use_so=False):
     
     relationship_location = get_relationship_location(locations)
@@ -11,28 +11,31 @@ def relation_net(locations, class_labels, relation_numbers, use_obj=True, use_so
        last_conv = vgg16(input_image, is_training=True)
 
     with tf.variable_scope('roi_pooling'):
-        x_u = roi_pooling(last_conv, relationship_location)
+        
+        x_u = roi_pooling(last_conv, relationship_location, 4, 4)
         x_u = tf.reshape(x_u, [x_u.get_shape.as_list()[0], -1])
-        x_u = tf.slim.fully_connected(x_u, 4096)
-        x_u = tf.slim.fully_connected(x_u, 4096)
+        x_u = tf.nn.relu(tf.slim.fully_connected(x_u, 4096))
+        x_u = tf.nn.relu(tf.slim.fully_connected(x_u, 4096))
         x_u = tf.slim.fully_connected(x_u, 256)        
 
         if use_so:
             x_so = roi_pooling(last_conv, location)
             x_so = tf.reshape(x_u, [x_so.get_shape.as_list()[0], -1])
 
-            x_so = tf.slim.fully_connected(x_so, 4096)
-            x_so = tf.slim.fully_connected(x_so, 4096)
+            x_so = tf.nn.relu(tf.slim.fully_connected(x_so, 4096))
+            x_so = tf.nn.relu(tf.slim.fully_connected(x_so, 4096))
             x_so = tf.slim.fully_connected(x_so, 256)
             x_u = tf.concat([x_u, x_so], 1)
+            
+    with tf.variable_scope('output'):
 
         lo = tf.slim.fully_connected(locations, 256)
 
         if use_obj:
             emb_so =tf.slim.fully_connected(class_labels, 256)
 
-    last = tf.concat([x_u, lo, emb_so], 1)
-    self.relation = tf.slim.fully_connected(last, relation_numbers)
+        last = tf.concat([x_u, lo, emb_so], 1)
+        relation = tf.slim.fully_connected(last, relation_numbers)
 
 def get_relationship_location(locations):
     pass
