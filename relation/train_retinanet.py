27718a5862b9d026605ee0bde968ab9ec18db95e
dowhilefalse <37575766+dowhilefalse@users.noreply.github.com>
2018-08-21 12:00:05 +0800

    Update train_retinanet.py

--- relation/train_retinanet.py
+++ relation/train_retinanet.py
@@ -38,69 +38,8 @@ def read_record(image_size=608):
     return images, labels
 
 
-# def train(model, trainloader, optimizer, device, epoch, criterion, step, logger, batch_size):
-#     encoder = DataEncoder()
-#     # http://qr.ae/TUIS14
-
-#     start_time = time.time()
-#     train_loss = 0
-
-#     for batch_idx, (data, loc_targets, cls_targets, ori_img_shape) in enumerate(trainloader):
-#         inputs, loc_targets, cls_targets = data.to(device), loc_targets.to(device), \
-#                 cls_targets.to(device)
-#         optimizer.zero_grad()
-#         loc_preds_split, cls_preds_split = model(inputs.cuda())
-#         loc_preds = torch.cat(loc_preds_split, 1)
-#         cls_preds = torch.cat(cls_preds_split, 1)
-#         loss, loc_loss, cls_loss=criterion(loc_preds.float(), loc_targets.cuda(), \
-#                 cls_preds.float(), cls_targets.cuda())
-        
-#         sess.run(op, )
-#         loss.backward()
-#         optimizer.step()
-
-        if batch_idx % 10 == 0:
-            # print loss & highest confidence
-            end_time = time.time()
-            print('[%d,%5d] cls_loss: %.5f loc_loss: %.5f train_loss: %.5f time: %.3f lr: %.6f' % \
-                    (epoch, batch_idx, cls_loss, loc_loss, loss,  \
-                    end_time-start_time, optimizer.param_groups[0]['lr']))
-            start_time = time.time()
-            # record loss
-            info = {'training loss': loss.item(), 'loc_loss': loc_loss.item(), \
-                    'cls_loss': cls_loss.item()}
-            for tag, value in info.items():
-                logger.scalar_summary(tag, value, step)
-
-#             # highest confidence
-#             t_cls_preds = torch.div(torch.sum(cls_preds, dim=0).squeeze(),batch_size)
-#             score, _ = tf.nn.sigmoid(t_cls_preds)
-#             obj_idx = score > 0.5 #  CONF_THRESH
-#             max_conf = tf.reduce_max(score)
-#             print('highest_conf: %f, num: %d' % (max_conf, torch.sum(obj_idx)))
-
-#         if batch_idx % 1 == 0:
-#             # summary image record
-#             pred_boxes, pred_labels = [], []
-#             if batch_size > 10:
-#                 show_num_img = 10
-#             else:
-#                 show_num_img = batch_size
-#             for img_idx in range(show_num_img):
-#                 pdb.set_trace()
-#                 pred_box, pred_label, _ = encoder.decode(loc_preds_split, cls_preds_split\
-#                     , data.shape, ori_img_shape[img_idx], img_idx)
-#                 pred_boxes.append(pred_box)
-#                 pred_labels.append(pred_label)
-#             info = {'images': inputs[:show_num_img].cpu().numpy()}
-#             for tag, images in info.items():
-#                 images = logger.image_drawbox(images, pred_boxes, pred_labels)
-#                 logger.image_summary(tag, images, step)
-#         step += 1
-#     return loss, step
-
-def test(model,testloader,device,criterion,logger,step):
-    model.eval()
+def test(input_image,testloader,device,criterion,logger,step):
+    
     for data, loc_targets, cls_targets, _ in testloader:
         inputs = data.to(device)
         loc_preds, cls_preds = model(inputs.cuda())
@@ -116,16 +55,6 @@ def test(model,testloader,device,criterion,logger,step):
     print('\nTest set: Test_loss: %.5f Test_cls_loss: %.5f Test_loc_loss: %.5f\n'%(loss, \
             cls_loss,loc_loss))
 
-def save_checkpoint(state,epoch,data_name):
-    path = "./weights/retina_{}_{}.pth".format(data_name,epoch)
-    torch.save(state,path)
-    print("Checkpoint saved to {}".format(path))
-
-def adjust_learning_rate(optimizer,lr,decay_idx):
-    for lr_idx in range(decay_idx+1):
-        lr /= 2
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
 
 
 def main():
@@ -151,6 +80,9 @@ def main():
     min_scale = 600
     max_scale = 1000
     image_size = 608
+    
+    
+    train_dir = 'model_path/'
 
     if args.debug == 'True':
         num_workers = 0 
@@ -165,11 +97,11 @@ def main():
     print("==>>Loading the data.....", args.data_name)
     num_classes = 62 #need to update
 
-    total_iter = 90000.
-    total_epoch = int(total_iter/len(trainloader)*len(gpus))
-    if args.data_name == 'OpenImagesDataset':
-        total_epoch += 30
-    print('==>>Total_epoch size is %d'%(total_epoch))
+    total_iter = 100000.
+#     total_epoch = int(total_iter/len(trainloader)*len(gpus))
+#     if args.data_name == 'OpenImagesDataset':
+#         total_epoch += 30
+#     print('==>>Total_epoch size is %d'%(total_epoch))
 
     lr_decay_method = args.lr_decay_method
     if lr_decay_method == 'luong5':
@@ -245,20 +177,27 @@ def main():
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
         
-        
-        for epoch in range(args.start_epoch, total_epoch):
+        start_time = time.time()
+        for iteration in range(args.start_iteration, total_iterations):
             img, las = sess.run([train_images, train_labels])
+            class_labels, location_labels = prepare_data(las)
             
             sess.run(op, feed_dict={input_image:, label_class:, label_loc:}))
-            if epoch % 10 == 0:
-                batch_loss = sess.run( loss, feed_dict={input_image:, label_class:, label_loc:})
-                print('Cost after epoch '+str(epoch)+':  ', batch_loss )
-    #         _, step = train(model, trainloader, optimizer, device, epoch, criterion, \
-    #                step, logger, batch_size)
-            if epoch % 50 == 0:
+            
+            if iteration % 10 == 0:
+                batch_loss = sess.run( loss, feed_dict={input_image: img, label_class: class_labels, label_loc: location_labels})
+                end_time = time.time()
+                
+                print('Cost after epoch '+str(epoch)+':  ', 'total_loss:', batch_loss, 'cls_loss:', batch_loss[0], \
+                      'loc_loss:', batch_loss[1], 'time_spent:', end_time-start_time )
+                info = {'training loss': batch_loss, 'loc_loss': batch_loss[0], \
+                    'cls_loss': batch_loss[1]}
+                for tag, value in info.items():
+                    logger.scalar_summary(tag, value, step)
+            if iteration % 10000 == 0:
                 test(model, testloader, device, criterion, logger, step)
 
-            if epoch % 500 == 0:
+            if iteration % 1000 == 0:
                 name = 'retinanet.ckpt'
                 saver.save( sess, os.path.join( traindir, name ), global_step = epoch)
 
