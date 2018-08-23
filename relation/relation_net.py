28fa10229516506ca13817277eb5b9bd28a19603
dowhilefalse <37575766+dowhilefalse@users.noreply.github.com>
2018-08-24 00:34:51 +0800

    Update relation_net.py

--- relation/relation_net.py
+++ relation/relation_net.py
@@ -2,7 +2,7 @@ from retinanet import *
 
 from roi_pooling.roi_pooling_ops import roi_pooling
 
-def relation_net(locations, class_labels, relation_numbers, use_obj=True, use_so=False):
+def relation_net(locations, class_labels, relation_numbers=9, texture_num=5, use_obj=True, use_so=False):
     
     relationship_location = get_relationship_location(locations)
 
@@ -18,14 +18,16 @@ def relation_net(locations, class_labels, relation_numbers, use_obj=True, use_so
         x_u = tf.nn.relu(tf.slim.fully_connected(x_u, 4096))
         x_u = tf.slim.fully_connected(x_u, 256)        
 
-        if use_so:
-            x_so = roi_pooling(last_conv, location)
-            x_so = tf.reshape(x_u, [x_so.get_shape.as_list()[0], -1])
+        
+        x_so = roi_pooling(last_conv, location)
+        x_so = tf.reshape(x_u, [x_so.get_shape.as_list()[0], -1])
 
-            x_so = tf.nn.relu(tf.slim.fully_connected(x_so, 4096))
-            x_so = tf.nn.relu(tf.slim.fully_connected(x_so, 4096))
-            x_so = tf.slim.fully_connected(x_so, 256)
-            x_u = tf.concat([x_u, x_so], 1)
+        x_so = tf.nn.relu(tf.slim.fully_connected(x_so, 4096))
+        x_so = tf.nn.relu(tf.slim.fully_connected(x_so, 4096))
+        x_so = tf.slim.fully_connected(x_so, 256)
+    with tf.variable_scope('output'):
+        emb_texture =tf.slim.fully_connected(class_labels, 256)
+        texture = tf.slim.fully_connected(tf.concat([x_so, emb_so], 1), texture_num)
             
     with tf.variable_scope('output'):
 
@@ -36,6 +38,8 @@ def relation_net(locations, class_labels, relation_numbers, use_obj=True, use_so
 
         last = tf.concat([x_u, lo, emb_so], 1)
         relation = tf.slim.fully_connected(last, relation_numbers)
+        
+    return relation, texture
 
 def get_relationship_location(locations):
     pass
