e7656e139490997db09b7311fa1128931ca7d783
dowhilefalse <37575766+dowhilefalse@users.noreply.github.com>
2018-09-03 01:43:46 +0800

    Update relation_net_loss.py

--- relation/relation_net_loss.py
+++ relation/relation_net_loss.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
 def texture_loss(pred_texture, labels_texture):
-     return tf.reduce_sum(tf.sigmoid_cross_entropy_with_logits(logits=pre_texture, labels=labels_texture))
+     return tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=pred_texture, labels=labels_texture))
 
 
 def relation_loss(pred_relations, labels_relations):
-     return tf.reduce_sum(tf.sigmoid_cross_entropy_with_logits(logits=pre_relations, labels=labels_relations))
+     return tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=pred_relations, labels=labels_relations))
